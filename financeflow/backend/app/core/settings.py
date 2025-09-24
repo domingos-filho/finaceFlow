15ff76809198d9c1ebd0db440ac3e8ffdb556be3
Francisco Antonio Domingos Filho <50499579+domingos-filho@users.noreply.github.com>
2025-09-23 21:31:07 -0300

    Update settings.py

--- financeflow/backend/app/core/settings.py
+++ financeflow/backend/app/core/settings.py
@@ -1,15 +1,19 @@
-
+import os
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
-    database_url: str = Field(default="postgresql+psycopg2://financeflow:financeflow123@db:5432/financeflow", alias="DATABASE_URL")
-    frontend_origin: str = Field(default="http://localhost:5173", alias="FRONTEND_ORIGIN")
-    jwt_secret: str = Field(default="dev-secret", alias="JWT_SECRET")
-    admin_email: str = Field(default="admin@local", alias="ADMIN_EMAIL")
-    admin_password: str = Field(default="Admin#123", alias="ADMIN_PASSWORD")
+    database_url: str = Field(
+        default=f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}",
+        alias="DATABASE_URL"
+    )
+    frontend_origin: str = Field(default=os.getenv("FRONTEND_ORIGIN"), alias="FRONTEND_ORIGIN")
+    jwt_secret: str = Field(default=os.getenv("JWT_SECRET"), alias="JWT_SECRET")
+    admin_email: str = Field(default=os.getenv("ADMIN_EMAIL"), alias="ADMIN_EMAIL")
+    admin_password: str = Field(default=os.getenv("ADMIN_PASSWORD"), alias="ADMIN_PASSWORD")
 
     class Config:
         extra = "ignore"
 
 settings = Settings()
+
