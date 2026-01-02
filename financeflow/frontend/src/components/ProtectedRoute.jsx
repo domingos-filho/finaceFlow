742bc4f1d220d380a520e5ef7425e99f7b07b16a
Francisco Antonio Domingos Filho <50499579+domingos-filho@users.noreply.github.com>
2026-01-02 15:02:31 -0300

    Delete financeflow directory

--- financeflow/frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-
-export default function ProtectedRoute() {
-  const { user, loading } = useAuth()
-  if (loading) return <div>Carregando...</div>
-  return user ? <Outlet /> : <Navigate to="/login" replace />
-}
